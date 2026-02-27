30fbe201e486dcab43e958c337f9bf5ff4b73f57
Mihir-Arjun-dvt <MArjun@dvtsoftware.com>
2026-02-27 13:57:14 +0200

    conversions now work

--- temp-converter/src/App.tsx
+++ temp-converter/src/App.tsx
@@ -3,25 +3,34 @@ import './index.css'
 import TemperatureConverter from './components/TemperatureConverter.tsx';
 
 function App() {
-  const [temperature, setTemperature] = useState('');
+  const [celcius, setCelcius] = useState('');
+  const [fahrenheit, setFahrenheit] = useState('');
 
-  function handleChange(e: ChangeEvent<HTMLInputElement>) {
-    setTemperature(e.target.value)
+  function handleCelciusConversion(e: ChangeEvent<HTMLInputElement>){
+    const temperature = e.target.value;
+    setCelcius(temperature);
+    setFahrenheit(temperature === '' ? '' : String((parseFloat(temperature) * 9/5) + 32));
+  }
+
+  function handleFahrenheitConversion(e: ChangeEvent<HTMLInputElement>){
+    const temperature = e.target.value;
+    setFahrenheit(temperature);
+    setCelcius(temperature === '' ? '' : String((parseFloat(temperature)-32)*(5/9)))
   }
 
   return (
     <>
       <TemperatureConverter
         label="Celcius: "
-        value={temperature}
-        tempChange={handleChange}
+        value={celcius}
+        tempChange={handleCelciusConversion}
       />
       <br></br>
       <br></br>
       <TemperatureConverter
         label="Fahrenheit: "
-        value={temperature}
-        tempChange={handleChange}
+        value={fahrenheit}
+        tempChange={handleFahrenheitConversion}
       />
     </>
   )
