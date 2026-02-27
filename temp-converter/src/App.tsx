0a07a9c11cba5070541253da8eca9eeda63fb633
Mihir-Arjun-dvt <MArjun@dvtsoftware.com>
2026-02-27 17:26:22 +0200

    added basic styling and set up shadcn

--- temp-converter/src/App.tsx
+++ temp-converter/src/App.tsx
@@ -1,6 +1,15 @@
 import { useState, type ChangeEvent } from 'react'
 import './index.css'
 import TemperatureConverter from './components/TemperatureConverter.tsx';
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 
 function App() {
   const [celsius, setCelsius] = useState('');
@@ -20,18 +29,33 @@ function App() {
 
   return (
     <>
-      <TemperatureConverter
-        label="Celsius: "
-        value={celsius}
-        tempChange={handleCelciusConversion}
-      />
-      <br></br>
-      <br></br>
-      <TemperatureConverter
-        label="Fahrenheit: "
-        value={fahrenheit}
-        tempChange={handleFahrenheitConversion}
-      />
+      <div className="flex justify-center gap-10 mt-20">
+        <Card className="w-80">
+          <CardHeader>
+            <CardTitle>Celsius</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <TemperatureConverter
+              label="Celsius: "
+              value={celsius}
+              tempChange={handleCelciusConversion}
+            />
+          </CardContent>
+        </Card>
+
+        <Card className="w-80">
+          <CardHeader>
+            <CardTitle>Fahrenheit</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <TemperatureConverter
+              label="Fahrenheit: "
+              value={fahrenheit}
+              tempChange={handleFahrenheitConversion}
+            />
+          </CardContent>
+        </Card>
+      </div>
     </>
   )
 }
