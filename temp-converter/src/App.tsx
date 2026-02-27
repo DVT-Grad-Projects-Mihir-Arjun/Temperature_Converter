e96233960f8e5f702ec57666d5451f439a2de3db
Mihir-Arjun-dvt <MArjun@dvtsoftware.com>
2026-02-27 13:29:29 +0200

    basic wiring done. Can reflect input from one into the other but need to now actually do the conversion

--- temp-converter/src/App.tsx
+++ temp-converter/src/App.tsx
@@ -1,20 +1,48 @@
+import { useState, type ChangeEvent } from 'react'
 import './index.css'
 
 function App() {
+  const [temperature, setTemperature] = useState('');
+
+  function handleChange(e: ChangeEvent<HTMLInputElement>) {
+    setTemperature(e.target.value)
+  }
 
   return (
     <>
-    <div>
-      <label>Celcius: </label>
-      <input type='text'></input>
-    </div>
-    <br></br>
-    <div>
-      <label>Fahrenheit: </label>
-      <input type='text'></input>
-    </div>
+      <TemperatureConverter
+        label="Celcius: "
+        value={temperature}
+        tempChange={handleChange}
+      />
+      <br></br>
+      <br></br>
+      <TemperatureConverter
+        label="Fahrenheit: "
+        value={temperature}
+        tempChange={handleChange}
+      />
     </>
   )
+}
+
+interface tempProps{
+  label: string;
+  value: string;
+  tempChange: React.ChangeEventHandler<HTMLInputElement>;
+}
+
+function TemperatureConverter({ label, value, tempChange }:tempProps) {
+  return (
+    <label>
+      {label}
+      {' '}
+      <input
+        value={value}
+        onChange={tempChange}
+      />
+    </label>
+  );
 }
 
 export default App
